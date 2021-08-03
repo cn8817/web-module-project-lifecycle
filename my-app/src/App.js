f0aae3b6210df7c45827a7bfd8f880667d0fc5ab
cn8817 <christinenguyen1433@gmail.com>
2021-08-03 16:29:17 -0700

    styled the usercard

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,42 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react'
 import axios from 'axios'
+import styled from 'styled-components'
+
+const StyledApp = styled.div`
+  img{
+    width:50%;
+    height:50%;
+    padding-top:5%;
+  }
+
+  .container{
+    display:flex;
+    border: 2px solid gray;
+    border-radius:10px;
+    padding:0;
+    margin:0 20%;
+
+  }
+
+  .imgContainer{
+    margin-top:3%;
+    padding-left:10%;
+  }
+  
+  .infoContainer{
+    padding:0 10%;
+  }
+
+  .follow{
+    display:flex;
+    justify-content: space-between;
+  }
+
+  p{
+    margin-right: 10%;
+  }
+`
 
 class App extends React.Component {
     state ={
@@ -26,13 +62,21 @@ class App extends React.Component {
     }
     render() {
         return(
-            <div>
-                <h1>Usercard</h1>
-                <h3>username: {this.state.username}</h3>
-                <img src ={this.state.avatar}/>
-                <p>followers: {this.state.followers}</p>
-                <p>following: {this.state.following}</p>
+          <StyledApp>
+            <h1>Usercard</h1>
+            <div class ='container'>
+                <div class='imgContainer'>
+                  <img src ={this.state.avatar}/>
+                </div>
+                <div class='infoContainer'>
+                  <h4>username: {this.state.username}</h4>
+                  <div class='follow'>
+                    <p>followers: {this.state.followers} </p>
+                    <p>following: {this.state.following} </p>
+                  </div>
+                </div>
             </div>
+            </StyledApp>
         )
     }
 }
